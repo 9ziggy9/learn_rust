72c334f07c7a2c82362643cd787bdf3a26472fa7
9ziggy9 <davidarogers@protonmail.com>
2023-07-06 16:32:48 -0700

    hello, rust

---
+++ hello_world/main.rs
@@ -0,0 +1,8 @@
+// $ rustc ./main.rs && ./main
+// >>> Hello, world!
+fn main() {
+    // Prints line, notice macro with !
+    // Not sure, but I think this is because
+    // there are no variadic functions in rust.
+    println!("Hello, world!");
+}
