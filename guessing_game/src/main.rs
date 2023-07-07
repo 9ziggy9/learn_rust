9e50356e219417bcac76806c2680629a1228d1f8
9ziggy9 <davidarogers@protonmail.com>
2023-07-06 18:22:39 -0700

    implemented better error handling on guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -25,48 +25,53 @@ fn main() {
     // enum type "Ordering" which has varieties: Less,
     // Greater, and Equal.
     loop {
-      println!("Please input your guess.");
-      // Notice how we declare variable as MUTABLE
-      // as references by default in Rust are IMMUTABLE
-      // String::new() is constructor of strings
-      // String type is  agorwable UTF-8 encoded bit of text
-      // new() in this case is an "associated function",
-      // that is a function which is implemented on a type.
-      let mut guess = String::new();
+        println!("Please input your guess.");
+        // Notice how we declare variable as MUTABLE
+        // as references by default in Rust are IMMUTABLE
+        // String::new() is constructor of strings
+        // String type is  agorwable UTF-8 encoded bit of text
+        // new() in this case is an "associated function",
+        // that is a function which is implemented on a type.
+        let mut guess = String::new();
 
-      // EXPERIMENT: why &mut? Notice compiler throws 
-      // type error when reference is not specifically
-      // labeled as mutable.
-      io::stdin()
-          .read_line(&mut guess)
-          .expect("Failed to read line"); // catch err
+        // EXPERIMENT: why &mut? Notice compiler throws 
+        // type error when reference is not specifically
+        // labeled as mutable.
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // catch err
 
-      // Need to change guess to u32 type, this takes
-      // only radix 10. Further, it is interesting to note
-      // that Rust compiler is INFERRING what type we want
-      // to parse to by the left-side assignment. We can
-      // also specify using ::<> syntax.
-      // TODO: take values of any radix
-      // let guess: u32 = guess
-      //     .trim()
-      //     .parse() // this is radix 10 by default
-      //     .expect("Please type a valid number!");
+        // Need to change guess to u32 type, this takes
+        // only radix 10. Further, it is interesting to note
+        // that Rust compiler is INFERRING what type we want
+        // to parse to by the left-side assignment. We can
+        // also specify using ::<> syntax.
+        // TODO: take values of any radix
+        // let guess: u32 = guess
+        //     .trim()
+        //     .parse() // this is radix 10 by default
+        //     .expect("Please type a valid number!");
 
-      let guess = guess // Note VARIABLE SHADOWING
-          .trim()
-          .parse::<u32>()
-          .expect("Please type a valid number!");
+        // Because parse returns result, we can match on
+        // Ok and Err; Ok holds correct value called num
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid radix 10 number!");
+                continue;
+            },
+        };
 
-      // String interpolation
-      println!("You guessed: {guess}");
+        // String interpolation
+        println!("You guessed: {guess}");
 
-      match guess.cmp(&secret_num) {
-          Ordering::Less    => println!("Too small!"),
-          Ordering::Greater => println!("Too big!"),
-          Ordering::Equal   => {
-              println!("You win!");
-              break;
-          }
-      }
+        match guess.cmp(&secret_num) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
