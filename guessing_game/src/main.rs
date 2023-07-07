ce6504e32fce9200a157bebd310f120cd235ed68
9ziggy9 <davidarogers@protonmail.com>
2023-07-06 19:14:58 -0700

    Added acceptance of command line arguments.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,8 @@
 // https://doc.rust-lang.org/std/prelude/index.html
 use std::io;
 use std::cmp::Ordering;
+use std::env;
+use std::process;
 
 // Note that we use ::Rng because Rng TRAIT defines
 // methods that random number generators implement.
@@ -9,9 +11,27 @@ use std::cmp::Ordering;
 // methods.
 use rand::Rng;
 
-fn main() {
-    println!("Guess the number!");
+const MSG_ARGS_TOO_FEW: &str = "Too few command line arguments.";
+const MSG_ARGS_TOO_MNY: &str = "Too many command line arguments.";
 
+fn min_max_from_args() -> [u32; 2] {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("ERROR IN ARGS: {}", MSG_ARGS_TOO_FEW);
+        process::exit(1);
+    }
+    if args.len() > 3 {
+        println!("ERROR IN ARGS: {}", MSG_ARGS_TOO_MNY);
+        process::exit(1);
+    }
+    return [
+        args[1].trim().parse().expect("Bad text input."),
+        args[2].trim().parse().expect("Bad text input.")];
+}
+
+fn main() {
+    let [_min, _max] = min_max_from_args();
+    println!("Guess the number!");
     // Random number from 1 to 100 inclusive
     // Note inclusivity of end guaranteed by =
     // thread_rng() gives us a random number generator local
@@ -19,8 +39,8 @@ fn main() {
     // After this, we call gen_range on this random number
     // generator.
     println!("Welcome to the guessing game!");
-    println!("Values range from 1 to 10.");
-    let secret_num = rand::thread_rng().gen_range(1..=10);
+    println!("Values range from {_min} to {_max}.");
+    let secret_num = rand::thread_rng().gen_range(_min..=_max);
 
     // Note that String type has a string comparison
     // method which we can utilize. It appears to return
@@ -58,11 +78,11 @@ fn main() {
         // Ok and Err; Ok holds correct value called num
 
         let guess = match guess.trim().parse::<u32>() {
-            Ok(num) if (1..=10).contains(&num) => num,
+            Ok(num) if (_min..=_max).contains(&num) => num,
             Ok(_)  => {
-                println!("Please enter a number between 1 and 10.");
+                println!("Please enter a number between {_min} and {_max}.");
                 continue;
-            }
+            },
             Err(_) => {
                 println!("Please enter a valid radix 10 number!");
                 continue;
