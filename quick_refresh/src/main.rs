a0517ff62d4dabcff26fe50897076b51902bce71
9ziggy9 <davidarogers@protonmail.com>
2023-09-04 06:43:57 -0700

    patern matching testing

--- quick_refresh/src/main.rs
+++ quick_refresh/src/main.rs
@@ -1,13 +1,12 @@
-use std::env::{self, Args};
-use std::iter::Enumerate;
+use std::env;
 
-const MSG_WELCOME: &str = "\nHello, welcome to command line experiments!";
+const MSG_WELCOME:     &str = "\nHello, welcome to command line experiments!";
+const ERR_BAD_ARG_NO:  &str = "BAD ASSERT: must supply 1 command line arg.";
 
-fn cmd_line_enumerate() -> Enumerate<Args> { env::args().enumerate() }
-
-fn cmd_line_to_string(args: Enumerate<Args>) -> String {
+fn cmd_line_to_string(args: Vec<String>) -> String {
     return args
-        .map(|(n, arg)| format!("Arg-{n}: {arg}"))
+        .iter().skip(1).enumerate()
+        .map(|(n, arg)| format!("arg {}: {arg}", n+1))
         .collect::<Vec<String>>()
         .join("\n");
 }
@@ -15,7 +14,9 @@ fn cmd_line_to_string(args: Enumerate<Args>) -> String {
 fn msg_intro(msgs: &[&str]) { msgs.iter().for_each(|m| println!("{m}")) }
 
 fn main() {
-    let args = cmd_line_enumerate();
+    // Equivalent: let args = env::args().collect::<Vec<String>>();
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2, "{}", ERR_BAD_ARG_NO);
     msg_intro(&[
         MSG_WELCOME,
         "\nYou entered the follow command line args:",
